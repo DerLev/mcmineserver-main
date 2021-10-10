44ba5e163dad9b3c951fd15ac0a3b22867b7d59a
DerLev <levin.schroeren@gmail.com>
2021-10-10 18:04:13 +0200

    added darkmode support and close button

--- components/StagingNote.tsx
+++ components/StagingNote.tsx
@@ -1,8 +1,21 @@
+import { XIcon } from '@heroicons/react/outline'
+import { useState } from 'react'
+
 function StagingNote() {
+  const [note, setNote] = useState(true);
+
   return (
-    <div className="bg-yellow-400 py-3 px-1 text-yellow-800">
-      <h1>You are viewing the latest beta/staging release of our website</h1>
-    </div>
+    <>
+     {
+       note &&
+      <div className="bg-yellow-400 py-3 px-5 text-yellow-800 flex justify-between">
+        <h1>You are viewing the latest beta/staging release of our website</h1>
+        <button onClick={() => setNote(!note)}>
+          <XIcon className="w-4 h-4" />
+        </button>
+      </div>
+     }
+    </>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'media',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: colors.gray
+      }
+    },
   },
   variants: {
     extend: {},
