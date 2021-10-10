850cd054db6084a939fbf7675513839f0422c6f0
DerLev <levin.schroeren@gmail.com>
2021-10-10 17:38:27 +0200

    added staging note

---
+++ components/StagingNote.tsx
@@ -0,0 +1,9 @@
+function StagingNote() {
+  return (
+    <div className="bg-yellow-400 py-3 px-1 text-yellow-800">
+      <h1>You are viewing the latest beta/staging release of our website</h1>
+    </div>
+  )
+}
+
+export default StagingNote
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,16 @@ import type { AppProps } from 'next/app'
 import { DefaultSeo } from 'next-seo'
 import SEO from '../seo.config.json'
 import ENV from '../env.json'
+import StagingNote from '../components/StagingNote'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <DefaultSeo dangerouslySetAllPagesToNoIndex={ENV.stagingEnv} dangerouslySetAllPagesToNoFollow={ENV.stagingEnv} {...SEO} />
+      {
+        ENV.stagingEnv &&
+        <StagingNote />
+      }
       <Component {...pageProps} />
     </>
   )
