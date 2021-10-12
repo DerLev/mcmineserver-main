38d9f8e0f0e57624e1baf460a762224ea46261dc
DerLev <levin.schroeren@gmail.com>
2021-10-12 23:00:50 +0200

    fixed linting error

--- pages/discord.tsx
+++ pages/discord.tsx
@@ -20,9 +20,9 @@ const nitroSvg = (level: number) => {
     case 1:
       return <svg className="w-4 h-4 text-discordNitro" width="512" height="512" viewBox="0 0 6 11"><path fill="currentColor" d="M3 0.625244L0 3.62524V7.62524L3 10.6252L6 7.62524V3.62524L3 0.625244ZM5 7.24524L3 9.24524L1 7.24524V4.04524L3 2.04524L5 4.04524V7.24524Z"></path></svg>
     case 2:
-      return <svg className="w-4 h-4 text-discordNitro" width="512" height="512" viewBox="0 0 6 11"><g fill="currentColor" fill-rule="evenodd"><path d="M2 4.42007V6.79007L3 7.79007L4 6.79007V4.42007L3.01 3.42007L2 4.42007Z"></path><path d="M3 0.590088L0 3.59009V7.59009L3 10.5901L6 7.59009V3.59009L3 0.590088ZM5 7.21009L3 9.21009L1 7.21009V4.00009L3 2.00009L5 4.00009V7.21009Z"></path></g></svg>
+      return <svg className="w-4 h-4 text-discordNitro" width="512" height="512" viewBox="0 0 6 11"><g fill="currentColor" fillRule="evenodd"><path d="M2 4.42007V6.79007L3 7.79007L4 6.79007V4.42007L3.01 3.42007L2 4.42007Z"></path><path d="M3 0.590088L0 3.59009V7.59009L3 10.5901L6 7.59009V3.59009L3 0.590088ZM5 7.21009L3 9.21009L1 7.21009V4.00009L3 2.00009L5 4.00009V7.21009Z"></path></g></svg>
     case 3:
-      return <svg className="w-4 h-4 text-discordNitro" width="512" height="512" viewBox="0 0 6 11"><g fill="currentColor" fill-rule="evenodd"><path d="M3 0.625305L0 3.62531V7.62531L3 10.6253L6 7.62531V3.62531L3 0.625305ZM5 7.24531L3 9.24531L1 7.24531V4.04531L3 2.04531L5 4.04531V7.24531Z"></path><path d="M3.76 4.21526L3 3.45526L2 4.45526V5.97526L3.76 4.21526Z"></path><path d="M2.28003 7.11532L3.00003 7.83532L4.00003 6.83532V5.39532L2.28003 7.11532Z"></path></g></svg>
+      return <svg className="w-4 h-4 text-discordNitro" width="512" height="512" viewBox="0 0 6 11"><g fill="currentColor" fillRule="evenodd"><path d="M3 0.625305L0 3.62531V7.62531L3 10.6253L6 7.62531V3.62531L3 0.625305ZM5 7.24531L3 9.24531L1 7.24531V4.04531L3 2.04531L5 4.04531V7.24531Z"></path><path d="M3.76 4.21526L3 3.45526L2 4.45526V5.97526L3.76 4.21526Z"></path><path d="M2.28003 7.11532L3.00003 7.83532L4.00003 6.83532V5.39532L2.28003 7.11532Z"></path></g></svg>
     default:
       break
   }
